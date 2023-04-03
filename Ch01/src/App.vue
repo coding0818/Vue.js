efc083dd230367da9081b8ac7238eab770f609e6
parkgayoung96 <lunatic2563@gmail.com>
2023-04-03 17:51:27 +0900

    2023/04/03

---
+++ Ch01/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h3>Ch01.Vue 소개</h3>
+  <p>Hello World</p>
+</template>
+
+<script>
+/**
+ * 날짜 : 2023/04/03
+ * 이름 : 박가영
+ * 내용 : Ch01.Vue.js 소개
+ * 
+ * 개발환경 설정
+ *  - node.js 설치
+ *  - vscode 설치 및 확장 플러그인(volar, vue3 snippets, prettier, ESLint)
+ *  - Vue.js devtools Chrome 확장플러그인 설치
+ * 
+ * 프로젝트 생성/실행
+ *  - npm init vite-app Ch01
+ *  - cd Ch01
+ *  - npm install(라이브러리 설치)
+ *  - npm run dev
+ * 
+ * 프로그램 번들링(배포파일 생성)
+ *  - npm run build
+ */
+
+
+export default {
+  name: 'App',
+}
+</script>
