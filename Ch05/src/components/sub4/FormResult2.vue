41210ac4852fe9fe9f699af55c20c23c2b9704b3
parkgayoung96 <lunatic2563@gmail.com>
2023-04-10 16:46:27 +0900

    2023/04/10

---
+++ Ch05/src/components/sub4/FormResult2.vue
@@ -0,0 +1,16 @@
+<template>
+  <h4>FormResult2</h4>
+  <p>
+    name : {{ user.name }}<br />
+    age : {{ user.age }}<br />
+    addr : {{ user.addr }}<br />
+  </p>
+</template>
+<script setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const user = route.params;
+</script>
+<style scoped></style>
