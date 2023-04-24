f27148981e45209bf14a7d1ee524931e51302b05
parkgayoung96 <lunatic2563@gmail.com>
2023-04-24 17:26:31 +0900

    2023/04/24

---
+++ Voard/src/components/Write.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-app-bar>
+      <v-app-bar-title>글쓰기</v-app-bar-title>
+      <v-btn @click="btnLogout" class="float-right">로그아웃</v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <v-sheet max-width="800" class="mx-auto"> </v-sheet>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+<script setup></script>
+<style scoped></style>
